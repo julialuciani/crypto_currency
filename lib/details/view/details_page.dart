54ba5f108daed35fd9bc7558c90551a0a064300e
julia <julia.luciani@warren.com>
2022-09-14 08:12:11 -0300

    feat: adding chart

--- lib/details/view/details_page.dart
+++ lib/details/view/details_page.dart
@@ -1,5 +1,6 @@
 import 'package:crypto/shared/utils/providers/crypto_provider.dart';
 import 'package:fl_chart/fl_chart.dart';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
