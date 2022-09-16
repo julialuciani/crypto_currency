d02ceef709f3ee10cac93d33da7562cd99e94160
julia <julia.luciani@warren.com>
2022-09-16 16:31:34 -0300

    feat: adding method walletBallance

--- lib/portifolio/widgets/upper_container_crypto.dart
+++ lib/portifolio/widgets/upper_container_crypto.dart
@@ -1,7 +1,10 @@
+import 'package:crypto/portifolio/controller/crypto_list_provider.dart';
 import 'package:crypto/portifolio/widgets/container_visible.dart';
+import 'package:crypto/shared/models/crypto_model.dart';
+import 'package:crypto/shared/utils/currency_formatter.dart';
+import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:intl/intl.dart';
 
 import '../../shared/style/colors.dart';
 import '../controller/visibility_provider.dart';
@@ -13,10 +16,22 @@ class UpperContainerCrypto extends StatefulHookConsumerWidget {
   ConsumerState<UpperContainerCrypto> createState() => _UpperCardCryptoState();
 }
 
+late CryptoModel cryptoModel;
+
 class _UpperCardCryptoState extends ConsumerState<UpperContainerCrypto> {
   @override
   Widget build(BuildContext context) {
     var visible = ref.watch(visibilityProvider.state);
+    var cryptosList = ref.read(cryptosListProvider);
+
+    Decimal walletBallance() {
+      Decimal balance = Decimal.parse('0');
+      for (int index = 0; index < cryptosList.length; index++) {
+        balance += cryptosList[index].howMuchUserHave;
+      }
+      return balance;
+    }
+
     return Container(
       height: 165,
       padding: const EdgeInsets.all(25),
@@ -49,8 +64,7 @@ class _UpperCardCryptoState extends ConsumerState<UpperContainerCrypto> {
           ),
           visible.state
               ? Text(
-                  NumberFormat.simpleCurrency(locale: 'pt-BR', decimalDigits: 2)
-                      .format(14798.00),
+                  FormatCurrency.format(walletBallance()),
                   style: const TextStyle(
                     fontSize: 29,
                     fontFamily: 'Montserrat',
