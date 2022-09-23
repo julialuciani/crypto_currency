b65445c4bd6a8d097548f588d4aec655abe4a715
julia <julia.luciani@warren.com>
2022-09-23 16:54:38 -0300

    feat: consuming api

--- lib/conversion/view/conversion_screen.dart
+++ lib/conversion/view/conversion_screen.dart
@@ -1,13 +1,15 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:projeto_crypto/details/widgets/details_app_bar.dart';
+import 'package:projeto_crypto/portifolio/usecase/cryptos_provider.dart';
 import 'package:projeto_crypto/shared/style/colors.dart';
 import 'package:projeto_crypto/shared/utils/currency_formatter.dart';
 
 import '../widgets/button_change_coin.dart';
 
-class ConversionScreen extends StatelessWidget {
+class ConversionScreen extends StatefulHookConsumerWidget {
   static const route = '/conversion';
   TextEditingController value = TextEditingController();
 
@@ -16,107 +18,136 @@ class ConversionScreen extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  ConsumerState<ConversionScreen> createState() => _ConversionState();
+}
+
+class _ConversionState extends ConsumerState<ConversionScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const DetailsAppBar(
-        title: 'Converter',
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              color: const Color.fromARGB(255, 245, 245, 250),
-              height: MediaQuery.of(context).size.height * 0.07,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Saldo disponível',
-                    style: TextStyle(fontSize: 17, color: Colors.grey.shade600),
+    final cryptos = ref.watch(cryptosProvider);
+    int first = 0;
+    int second = 1;
+
+    return cryptos.when(
+      data: (data) {
+        return Scaffold(
+          appBar: const DetailsAppBar(
+            title: 'Converter',
+          ),
+          body: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  color: const Color.fromARGB(255, 245, 245, 250),
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Saldo disponível',
+                        style: TextStyle(
+                            fontSize: 17, color: Colors.grey.shade600),
+                      ),
+                      Text(
+                        '0.5 ${data[1].symbol}',
+                        style: const TextStyle(fontSize: 17),
+                      ),
+                    ],
                   ),
-                  const Text(
-                    '0,65 BTC',
-                    style: TextStyle(fontSize: 17),
+                ),
+                const SizedBox(height: 30),
+                const AutoSizeText(
+                  'Quanto você gostaria de converter?',
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 30),
-            const AutoSizeText(
-              'Quanto você gostaria de converter?',
-              maxLines: 2,
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                ButtonChangeCoin(
-                  image: 'assets/icons/bitecoin.png',
-                  abbreviation: 'BTC',
                 ),
-                Icon(
-                  Icons.compare_arrows,
-                  color: magenta,
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ButtonChangeCoin(
+                      image: data[first].image,
+                      abbreviation: data[first].symbol.toUpperCase(),
+                      cryptos: data,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.compare_arrows, color: magenta),
+                      onPressed: () {
+                        setState(() {
+                          first = 1;
+                          second = 2;
+                        });
+                      },
+                    ),
+                    ButtonChangeCoin(
+                      image: data[second].image,
+                      abbreviation: data[second].symbol.toUpperCase(),
+                      cryptos: data,
+                    ),
+                  ],
                 ),
-                ButtonChangeCoin(
-                  image: 'assets/icons/ethereum.png',
-                  abbreviation: 'ETH',
+                TextFormField(
+                  showCursor: false,
+                  controller: widget.value,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: magenta,
+                        width: 3,
+                      ),
+                    ),
+                    focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: magenta, width: 3)),
+                    hintText: 'BTC 0,00',
+                    hintStyle: TextStyle(
+                      fontSize: 30,
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  FormatCurrency.format(0.00),
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey.shade500,
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.32),
+                const Divider(thickness: 1),
+                Text(
+                  'Total estimado',
+                  style: TextStyle(color: Colors.grey.shade500, fontSize: 17),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  '0,0000 ETH',
+                  style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
                 ),
               ],
             ),
-            TextFormField(
-              showCursor: false,
-              controller: value,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                enabledBorder: const UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: magenta,
-                    width: 3,
-                  ),
-                ),
-                focusedBorder: const UnderlineInputBorder(
-                    borderSide: BorderSide(color: magenta, width: 3)),
-                hintText: 'BTC 0,00',
-                hintStyle: TextStyle(
-                  fontSize: 30,
-                  color: Colors.grey.shade500,
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              FormatCurrency.format(0.00),
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.grey.shade500,
-              ),
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.32),
-            const Divider(thickness: 1),
-            Text(
-              'Total estimado',
-              style: TextStyle(color: Colors.grey.shade500, fontSize: 17),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              '0,0000 ETH',
-              style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color.fromARGB(255, 202, 200, 212),
-        onPressed: () {},
-        child: const Icon(Icons.navigate_next),
-      ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: const Color.fromARGB(255, 202, 200, 212),
+            onPressed: () {},
+            child: const Icon(Icons.navigate_next),
+          ),
+        );
+      },
+      error: (error, stackTrace) {
+        return const Text('Deu erro');
+      },
+      loading: () {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
--- lib/conversion/widgets/button_change_coin.dart
+++ lib/conversion/widgets/button_change_coin.dart
@@ -1,64 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ButtonChangeCoin extends StatelessWidget {
-  final String image;
+import 'package:projeto_crypto/portifolio/usecase/cryptos_provider.dart';
+
+class ButtonChangeCoin extends HookConsumerWidget {
+  final dynamic image;
   final String abbreviation;
+  final List? cryptos;
   const ButtonChangeCoin({
     Key? key,
     required this.image,
     required this.abbreviation,
+    required this.cryptos,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(25),
-          side: BorderSide(color: Colors.grey.shade300)),
-      onPressed: () {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            elevation: 2,
-            backgroundColor: Colors.white,
-            content: SizedBox(
-              height: 200,
-              child: ListView.separated(
-                separatorBuilder: (context, index) =>
-                    const Divider(thickness: 1),
-                itemCount: 2,
-                itemBuilder: (context, index) {
-                  return InkWell(
-                    onTap: () {},
-                    child: const ListTile(
-                      title: Text('ETH'),
-                      subtitle: Text('Ethereum'),
-                      trailing: Icon(
-                        Icons.arrow_forward_ios,
-                        size: 15,
-                      ),
-                    ),
-                  );
-                },
+  Widget build(BuildContext context, WidgetRef ref) {
+    var cryptos = ref.watch(cryptosProvider);
+
+    return cryptos.when(
+      data: (data) {
+        return MaterialButton(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25),
+              side: BorderSide(color: Colors.grey.shade300)),
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                elevation: 2,
+                backgroundColor: Colors.white,
+                content: SizedBox(
+                  height: 200,
+                  child: ListView.separated(
+                    separatorBuilder: (context, index) =>
+                        const Divider(thickness: 1),
+                    itemCount: 2,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {},
+                        child: const ListTile(
+                          title: Text('ETH'),
+                          subtitle: Text('Ethereum'),
+                          trailing: Icon(
+                            Icons.arrow_forward_ios,
+                            size: 15,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
               ),
-            ),
+            );
+          },
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 16,
+                backgroundImage: AssetImage(
+                  image,
+                ),
+              ),
+              const SizedBox(width: 8),
+              Text(abbreviation),
+              const SizedBox(width: 2),
+              const Icon(Icons.expand_more),
+            ],
           ),
         );
       },
-      child: Row(
-        children: [
-          CircleAvatar(
-            radius: 16,
-            backgroundImage: AssetImage(
-              image,
-            ),
-          ),
-          const SizedBox(width: 8),
-          Text(abbreviation),
-          const SizedBox(width: 2),
-          const Icon(Icons.expand_more),
-        ],
-      ),
+      error: (error, stackTrace) {
+        debugPrint(stackTrace.toString());
+        debugPrint(error.toString());
+        return const Text('Deu erro');
+      },
+      loading: () {
+        return const CircularProgressIndicator();
+      },
     );
   }
 }
