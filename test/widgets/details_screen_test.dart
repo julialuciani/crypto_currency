b7d775f60cb991aa1ae9822f5301c56a990b084d
julia <julia.luciani@warren.com>
2022-10-04 11:30:09 -0300

    feat: adding test on upper Column crypto on details

--- test/widgets/details_screen_test.dart
+++ test/widgets/details_screen_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 import 'package:projeto_crypto/details/widgets/change_days_button.dart';
 import 'package:projeto_crypto/details/widgets/chart_button.dart';
+import 'package:projeto_crypto/details/widgets/upper_column_crypto.dart';
 import 'package:projeto_crypto/shared/templates/button_default_app.dart';
 import 'package:projeto_crypto/shared/utils/app_arguments.dart';
 import 'package:projeto_crypto/shared/utils/app_bar_default.dart';
@@ -16,6 +18,19 @@ void main() {
 
       final appBarFinder = find.byType(AppBar);
       expect(appBarFinder, findsOneWidget);
+    });
+
+    testWidgets('Testing upper container', (WidgetTester tester) async {
+      mockNetworkImagesFor(() async {
+        await loadPage(tester, UpperColumnCrypto(crypto: crypto));
+        await tester.pumpAndSettle();
+
+        final textFinder = find.byType(Text);
+        final imageFinder = find.byType(Image);
+
+        expect(textFinder, findsAtLeastNWidgets(3));
+        expect(imageFinder, findsOneWidget);
+      });
     });
 
     testWidgets('Testing buttons chart', (WidgetTester tester) async {
