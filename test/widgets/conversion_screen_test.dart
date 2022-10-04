ec9bb9ef3f26b443ad4e0c186a6711b4229ef0b4
julia <julia.luciani@warren.com>
2022-10-04 12:55:21 -0300

    feat: adding test on button change coin

--- test/widgets/conversion_screen_test.dart
+++ test/widgets/conversion_screen_test.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+import 'package:projeto_crypto/conversion/widgets/button_change_coin.dart';
 import 'package:projeto_crypto/conversion/widgets/interactive_text.dart';
 import 'package:projeto_crypto/conversion/widgets/total_container.dart';
 import 'package:projeto_crypto/conversion/widgets/upper_container_conversion.dart';
+import 'package:projeto_crypto/portifolio/widgets/listview_crypto.dart';
 
 import '../helpers/crypto_mock_data.dart';
 import '../helpers/setup_widget_tester.dart';
@@ -33,6 +36,31 @@ void main() {
       await loadPage(tester, const InteractiveText());
       final textFinder = find.byType(InteractiveText);
       expect(textFinder, findsOneWidget);
+    });
+
+    testWidgets('Testing change coin button has all it needs',
+        (WidgetTester tester) async {
+      mockNetworkImagesFor(() async {
+        await loadPage(
+            tester,
+            ButtonChangeCoin(
+                crypto: crypto,
+                data: [crypto, crypto],
+                listView: const ListViewCryptos()));
+        await tester.pumpAndSettle();
+
+        final sizedBoxFinder = find.byType(SizedBox);
+        final textFinder = find.byType(Text);
+        final iconFinder = find.byIcon(Icons.expand_more);
+        final imageFinder = find.byType(Image);
+        final circleAvatarFinder = find.byType(CircleAvatar);
+
+        expect(textFinder, findsOneWidget);
+        expect(iconFinder, findsOneWidget);
+        expect(imageFinder, findsOneWidget);
+        expect(circleAvatarFinder, findsOneWidget);
+        expect(sizedBoxFinder, findsAtLeastNWidgets(2));
+      });
     });
   });
 }
